80c6d01fef489ceefe1485e1d6a162c021623158
Wen Jie <wenjie.sing@laputa.com.my>
2018-11-13 17:31:28 +0800

    integrating operation arguments

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,30 +1,43 @@
 const Query = {
     users(parent, args, {prisma}, info){
-        return prisma.query.users(null,info)
+        const opArgs = {}
 
-        // if(!args.query) {
-        //     return db.users
-        // }
+        if(args.query) {
+            opArgs.where = {
+                OR: [{
+                    name_contains: args.query
+                },{
+                    email_contains: args.query
+                }]//query:"@input"
+            }
+        }
 
-        // return db.users.filter((user) => {
-        //     return user.name.toLowerCase().includes(args.query.toLowerCase())
-        // });
+        return prisma.query.users(opArgs,info)
     },
     posts(parent, args, {prisma}, info){
-        return prisma.query.posts(null,info)
+        const opArgs = {}
 
-        // if(!args.query) {
-        //     return db.posts
-        // }
+        if(args.query) {
+            opArgs.where = {
+                OR: [{
+                    title_contains: args.query
+                },{
+                    body_contains: args.query
+                }]//query:"@input"
+            }
+        }
 
-        // return db.posts.filter((post) => {
-        //     return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
-        // });
+        return prisma.query.posts(opArgs,info)
     },
     comments(parent, args, {prisma}, info){
-        return prisma.query.comments(null,info)
+        const opArgs = {}
 
-        // return db.comments
+        if(args.query) {
+            opArgs.where = {
+                text_contains: args.query
+            }
+        }
+        return prisma.query.comments(opArgs,info)
     }
 }
 
