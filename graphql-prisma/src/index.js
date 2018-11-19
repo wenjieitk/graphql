5d67b67e0d92169f05e08c0b3e42fcbbe3c4a5ea
Wen Jie <wenjie.sing@laputa.com.my>
2018-11-19 15:52:35 +0800

    graphql fragment to lock down individual types

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,31 +1,20 @@
 import {GraphQLServer, PubSub} from 'graphql-yoga';
-import Query from './resolvers/Query';
-import Mutation from './resolvers/Mutation'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
-import Subscription from './resolvers/Subscription'
 import prisma from './prisma'
+import {resolvers, fragmentReplacements} from './resolvers/index'
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers: {
-        Query,
-        Mutation,
-        Subscription,
-        User,
-        Post,
-        Comment
-    },
+    resolvers,
     context(request) {
         return {
             pubsub,
             prisma,
             request
         }
-    }
+    },
+    fragmentReplacements
 });
 
 server.start(() =>{
--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,5 +1,45 @@
-const User = {
+import getUserId from '../utils/getUserId'
+import prisma from '../prisma';
 
+const User = {
+    // email(parent, args, {request}, info) {
+    //     const userid = getUserId(request, false)
+
+    //     console.log("parent: \n" + JSON.stringify(parent))
+
+    //     if(userid && userid === parent.id) {
+    //         return parent.email
+    //     }else{
+    //         return null
+    //     }
+    // }
+
+    email: {
+        fragment: `fragment userId on User {id}`,
+        resolve (parent, args, {request}, info) {
+            const userid = getUserId(request, false)
+        
+            if(userid && userid === parent.id) {
+                return parent.email
+            }else{
+                return null
+            }
+        }
+    },
+
+    posts:{
+        fragment: 'fragment userId on User {id}',
+        resolve (parent, args, {request}, info) {
+            return prisma.query.posts({
+                where: {
+                    published:true,
+                    author: {
+                        id: parent.id
+                    }
+                }
+            })
+        }
+    }
 }
 
 export {User as default}
